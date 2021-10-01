9ea58658ca1c39ffe278bf713cce03f9e4f91955
Kanchev <kanchev.veselin@gmail.com>
2021-10-01 12:32:58 +0100

    up

---
+++ build/precache-manifest.fcb2f0713ed2f9295bf5c9afbba8e18e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d4942e22c2fd905b81c94c5118245b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9beef5afd3bb59928f49",
+    "url": "/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "c58dd89c850d9dee572c",
+    "url": "/static/css/main.4341def2.chunk.css"
+  },
+  {
+    "revision": "9beef5afd3bb59928f49",
+    "url": "/static/js/2.9088103d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.9088103d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c58dd89c850d9dee572c",
+    "url": "/static/js/main.6dda4def.chunk.js"
+  },
+  {
+    "revision": "1535ba1dc7849a9b90e4",
+    "url": "/static/js/runtime-main.4220ea81.js"
+  }
+]);
